63c9965ba9c87ab1ba285574f451e7dd76927161
deRockerTom <tom.derockerpro@gmail.com>
2025-04-02 19:08:43 +0200

    feat(frontend): Add version control sidebar with mocked versions

--- frontend/src/pages/Note/Note.tsx
+++ frontend/src/pages/Note/Note.tsx
@@ -8,6 +8,30 @@ import { useLayoutContext } from "@components/Layout.helpers";
 import SaveNote from "./components/SaveNote";
 import VersionControlButton from "./components/VersionControlButton";
 import "./Note.scss";
+import classNames from "classnames";
+
+const versions = [
+  {
+    date: new Date().toISOString(),
+    version: 3,
+    note_id: "1",
+  },
+  {
+    date: new Date(Date.now() - 1000 * 60 * 60 * 24).toISOString(),
+    version: 2,
+    note_id: "1",
+  },
+  {
+    date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2).toISOString(),
+    version: 1,
+    note_id: "1",
+  },
+  {
+    date: new Date(Date.now() - 1000 * 60 * 60 * 24 * 3).toISOString(),
+    version: 0,
+    note_id: "1",
+  },
+];
 
 function Note() {
   const noteId = useRequiredParam("noteId");
@@ -18,6 +42,8 @@ function Note() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const byPassPromptOnChangeRef = useRef(false);
+  const [isVersionControlVisible, setIsVersionControlVisible] = useState(false);
+  const [selectedVersion, setSelectedVersion] = useState(0);
 
   const handleDeleteNoteClick = () => {
     byPassPromptOnChangeRef.current = true;
@@ -68,53 +94,87 @@ function Note() {
 
   return (
     <div className="note">
-      <div className="note__header">
-        <input
-          type="text"
-          value={title}
-          onChange={handleTitleChange}
-          className="note__header__title"
-          placeholder="Note Title"
+      <div className="note__main">
+        <div className="note__main__header">
+          <input
+            type="text"
+            value={title}
+            onChange={handleTitleChange}
+            className="note__main__header__title"
+            placeholder="Note Title"
+          />
+          <VersionControlButton
+            onClick={() => {
+              console.log("Version Control Clicked");
+              setIsVersionControlVisible((prev) => !prev);
+            }}
+          />
+          <DeleteNote
+            noteId={noteId}
+            className="note__main__header__trash-can"
+            onDelete={handleDeleteNoteClick}
+          />
+        </div>
+        <textarea
+          value={content}
+          onChange={handleContentChange}
+          className="note__main__content"
+          placeholder="Note Content"
         />
-        <VersionControlButton
-          onClick={() => {
-            console.log("Version Control Clicked");
-          }}
-        />
-        <DeleteNote
+        <SaveNote
           noteId={noteId}
-          className="note__header__trash-can"
-          onDelete={handleDeleteNoteClick}
+          content={content}
+          title={title}
+          onSave={() => {
+            // Here we assume that the note wasn't modified after in the backend
+            // and we can just update the local state
+            // We could also check the backend for the latest version of the note
+            // instead
+            setRemoteNote((prev) => {
+              if (!prev) return prev;
+              return {
+                ...prev,
+                title,
+                content,
+              };
+            });
+          }}
+          disabled={
+            title === remoteNote?.title && content === remoteNote?.content
+          }
         />
       </div>
-      <textarea
-        value={content}
-        onChange={handleContentChange}
-        className="note__content"
-        placeholder="Note Content"
-      />
-      <SaveNote
-        noteId={noteId}
-        content={content}
-        title={title}
-        onSave={() => {
-          // Here we assume that the note wasn't modified after in the backend
-          // and we can just update the local state
-          // We could also check the backend for the latest version of the note
-          // instead
-          setRemoteNote((prev) => {
-            if (!prev) return prev;
-            return {
-              ...prev,
-              title,
-              content,
-            };
-          });
-        }}
-        disabled={
-          title === remoteNote?.title && content === remoteNote?.content
-        }
-      />
+      <div
+        className={classNames("note__sidebar", {
+          "note__sidebar--collapsed": !isVersionControlVisible,
+        })}
+      >
+        {isVersionControlVisible && (
+          <>
+            <div className="note__sidebar__title">Versions</div>
+            <div className="note__sidebar__version-list">
+              {versions.map((version) => (
+                <span
+                  key={version.version}
+                  className={classNames("note__sidebar__version-list__item", {
+                    "note__sidebar__version-list__item--active":
+                      version.version === selectedVersion,
+                  })}
+                  onClick={() => {
+                    console.log("Version Clicked", version.version);
+                    setSelectedVersion(version.version);
+                  }}
+                >
+                  {new Date(version.date).toLocaleDateString(undefined, {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </span>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
