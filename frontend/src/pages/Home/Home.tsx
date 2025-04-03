6259718df26dc6b28e16c982bebfad4ac27d287e
deRockerTom <tom.derockerpro@gmail.com>
2025-04-03 23:34:35 +0200

    chore(frontend): Better home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,8 +3,17 @@ import "./Home.scss";
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to the Note App</h1>
-      <p>Select a note from the left sidebar to view its content.</p>
+      <div className="home__welcome-message">
+        <h1 className="home__welcome-message__header">
+          Welcome to the Note App
+        </h1>
+        <p className="home__welcome-message__subheader">
+          Select a note from the left sidebar to view its content.
+        </p>
+      </div>
+      <div className="home__instruction">
+        <p>Start writing, organizing, and tracking your notes easily!</p>
+      </div>
     </div>
   );
 }
